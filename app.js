61e79b63936946f847ead0190ab705a7af409318
Isadora Bastos <isadora.bastos@engenharia.ufjf.br>
2020-01-23 21:05:37 -0300

    initial commit

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const sha1 = require('sha1');
 
 const app = express();
 
@@ -11,16 +12,67 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function(request, response) {
-  var medicao = request.query.medicao;
-  var timestamp = request.query.timestamp;
-  response.send("medição: " + medicao+ " timestamp: " +timestamp);
+  var x1 = request.query.x1; //idpublica
+  var x2 = request.query.x2; //tsta
+  var x3 = request.query.x3; //body
+  var x4 = 654321; //idprivada
+  var hash = sha1(x2+x3+x4);
+  if(hash==request.hash){
+  console.log("ok");
+}
+ else {
+   console.log("ERROR");
+ }
+    });
+ 
+
+
+app.post("/signup", function(req, res) {
+  var firstName = req.body.fName;
+  var lastName = req.body.lName;
+  var email = req.body.email;
+
+  var data = {
+    members: [
+      {email_address: email,
+      status: "subscribed",
+      merge_fields: {
+        FNAME: firstName,
+        LNAME: lastName
+      }
+    }
+    ]
+  };
+
+  var jsonData = JSON.stringify(data);
+
+  var options = {
+  url:"https://us5.api.mailchimp.com/3.0/lists/58ba608dce",
+  method:"POST",
+  headers: {
+    "Authorization": "isapbastos 1b99f508aa0c0f767d582906210d3000-us5"
+  },
+body: jsonData
+  };
+
+  request(options, function(error, response, body){
+    if (error) {
+      res.sendFile(__dirname + "/failure.html");
+      } else {
+        if(response.statusCode ==200){
+          res.sendFile(__dirname + "/success.html");
+        } else {
+          res.sendFile(__dirname + "/failure.html");
+        }
+      }
+});
 });
 
-app.get("/", function(req, res) {
+app.get("/signup", function(req, res) {
   res.sendFile(__dirname + "/signup.html");
 });
 
-app.post("/", function(req, res) {
+app.post("/signup", function(req, res) {
   var firstName = req.body.fName;
   var lastName = req.body.lName;
   var email = req.body.email;
