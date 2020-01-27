37e22bfb0cd3b5db42fc7f1f678a9834fd239b14
Isadora Bastos <isadora.bastos@engenharia.ufjf.br>
2020-01-27 14:01:08 -0300

    signup

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const request = require("request");
 const sha1 = require("sha1");
 const https = require('https');
 const fs = require('fs');
+const mongoose = require('mongoose');
 const app = express();
 
 app.use(express.static("public"));
@@ -23,15 +24,16 @@ const options = {
 };
 
 
-{[{'idPublica':'123456', 'idPrivada':'654321'},
-{'idPublica':'abe854', 'idPrivada':'fk9W21'} ]}
+//{[{'idPublica':'123456', 'idPrivada':'654321'},
+//{'idPublica':'abe854', 'idPrivada':'fk9W21'} ]}
 
 app.get('/', function(request, response) {
     var x1 = request.query.x1; //timeStamp
     var x2 = request.query.x2; //body
     var x3a = request.query.x3a; //idpublica
-    var x3b = JSON.stringify(x3a).replace('"123456"', '654321').replace('"abe854"', 'fk9W21').replace('"anr5kr"', 'f93jc2').replace('"dnuek2"', '29jd90');
+    var x3b = JSON.stringify(x3a).replace('"123456"', '654321').replace('"abe854"', 'fk9W21').replace('"anr5kr"', 'f93jc2').replace('"dnuek2"', '29jd90'); //faz a conversão da idpública para idprivada
     var x4 = request.query.x4;
+    
     var hash = sha1(x1+x2+x3b); //função para gerar hash sha1
     if(x4==hash){ //comparacao do hash. Se igual, aceita a mensagem, caso contrario rejeita
         response.send(MSG_CONEXAO_ACEITA);
@@ -45,53 +47,52 @@ app.get('/', function(request, response) {
 
 app.get("/signup", function(req, res) {
     res.sendFile(__dirname + "/signup.html");
-});
-
- app.post("/signup", function(req, res) {
+  });
+  
+  app.post("/", function(req, res) {
     var firstName = req.body.fName;
     var lastName = req.body.lName;
     var email = req.body.email;
-
+  
     var data = {
-        members: [{
-            email_address: email,
-            status: "subscribed",
-            merge_fields: {
-                FNAME: firstName,
-                LNAME: lastName
-            }
-        }]
-    };
-
-    var jsonData = JSON.stringify(data);
-
-    var options = {
-        url:"https://us5.api.mailchimp.com/3.0/lists/58ba608dce",
-        method:"POST",
-        headers: {
-            "Authorization": "isapbastos 1b99f508aa0c0f767d582906210d3000-us5"
-        },
-
-        body: jsonData
-    };
-
-    request(options, function(error, response, body){
-        if (error) {
-            res.sendFile(__dirname + "/failure.html");
-        } else {
-            if(response.statusCode == ALL_OKAY){
-                res.sendFile(__dirname + "/success.html");
-            } else {
-                res.sendFile(__dirname + "/failure.html");
-            }
+      members: [
+        {email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
         }
-    });
-});
-
-
-app.post("/failure", function(req, res){
-    res.redirect("/");
-});
+      }
+      ]
+    };
+  
+    var jsonData = JSON.stringify(data);
+  
+    var options = {
+    url:"https://us5.api.mailchimp.com/3.0/lists/58ba608dce",
+    method:"POST",
+    headers: {
+      "Authorization": "isapbastos 1b99f508aa0c0f767d582906210d3000-us5"
+    },
+  body: jsonData
+    };
+  
+    request(options, function(error, response, body){
+      if (error) {
+        res.sendFile(__dirname + "/failure.html");
+        } else {
+          if(response.statusCode == ALL_OKAY){
+            res.sendFile(__dirname + "/success.html");
+          } else {
+            res.sendFile(__dirname + "/failure.html");
+          }
+        }
+  });
+  });
+  
+  app.post("/failure", function(req, res){
+    res.redirect("/signup");
+  });
 
 app.listen(process.env.PORT || PORTA_LOCAL_HTTP, function() {
     console.log("Server is running in port 3000!");
@@ -99,7 +100,7 @@ app.listen(process.env.PORT || PORTA_LOCAL_HTTP, function() {
 
 https.createServer(options, function (req, res) {
     res.writeHead(ALL_OKAY);
-    res.end("Server is running in port 8000!\n");
+    console.log("Server is running in port 8000!\n");
 }).listen(PORTA_LOCAL_HTTPS);
 
 //API key
